8e33f95ef3603415fc9dfd1b62361f1589a111a1
sayalikeche <kechesayali11@gmail.com>
2021-11-14 21:50:58 +0530

    [sayali Added CompareTwoLine]

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -1,18 +1,18 @@
 ﻿using System;
-
-namespace LineComparision
+namespace LineComparison
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparision Computation Program");
-            LineCalculation ln = new LineCalculation(9.0, 1.0, 3.0, 5.0);
-            double lengthOne = ln.lengthCalculation();
-            Console.WriteLine("The Length of the First Line is {0}", lengthOne);
-            double lengthTwo = ln.lengthCalculation();
-            Console.WriteLine("The Length of Second Line is {0}", lengthTwo);
-            ln.Check(lengthOne, lengthTwo);
+            Console.WriteLine("Welcome to Line Comparison Computation Progam");
+            LineComparison lineOne = new LineComparison(1.5, 2.5, 3.5, 4.5);
+            double lengthOne = lineOne.LengthCalc();
+            Console.WriteLine("The length of the first line is {0}", lengthOne);
+            LineComparison lineTwo = new LineComparison(2.5, 9.5, 7.5, 5.5);
+            double lengthTwo = lineTwo.LengthCalc();
+            Console.WriteLine("The length of the second line is {0}", lengthTwo);
+            lineTwo.Check(lengthOne, lengthTwo);
         }
     }
 }
