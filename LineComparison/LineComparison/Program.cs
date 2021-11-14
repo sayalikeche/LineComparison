f25da696e7c249d87f2bc44e5868017da8be3368
sayalikeche <kechesayali11@gmail.com>
2021-11-14 21:38:44 +0530

    [sayali Added Distance Between Two Line]

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -1,13 +1,32 @@
 ﻿using System;
 
-namespace LineComparison
+namespace CalculateDistanceBetweenTwoPointsofLine
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison");
-            Console.ReadLine();
+            String X1, X2, Y1, Y2;
+
+            int a1, b1, a2, b2;
+            Console.WriteLine("Enter the Co-ordinates of first point as A(x1,y1) : ");
+            X1 = Console.ReadLine();
+            a1 = Convert.ToInt32(X1);
+            Y1 = Console.ReadLine();
+            b1 = Convert.ToInt32(Y1);
+            Console.WriteLine("Enter the Co-ordinates of second point as B(x2,y2) : ");
+            X2 = Console.ReadLine();
+            a2 = Convert.ToInt32(X2);
+            Y2 = Console.ReadLine();
+            b2 = Convert.ToInt32(Y2);
+
+            Console.WriteLine("The Distance between two points is : " + Math.Round(distance(a1, b1, a2, b2) * 100000.0) / 100000.0);
         }
+        static double distance(int x1, int y1, int x2, int y2)
+        {
+            
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) * 1.0);
+        }
+
     }
 }
