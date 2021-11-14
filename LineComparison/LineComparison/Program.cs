44654207d2d92dfcd8d858bc65ec7bbe6383a532
sayalikeche <kechesayali11@gmail.com>
2021-11-14 21:44:24 +0530

    [sayali Added EqualLength]

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -1,32 +1,18 @@
 ﻿using System;
 
-namespace CalculateDistanceBetweenTwoPointsofLine
+namespace LineComparision
 {
     class Program
     {
         static void Main(string[] args)
         {
-            String X1, X2, Y1, Y2;
-
-            int a1, b1, a2, b2;
-            Console.WriteLine("Enter the Co-ordinates of first point as A(x1,y1) : ");
-            X1 = Console.ReadLine();
-            a1 = Convert.ToInt32(X1);
-            Y1 = Console.ReadLine();
-            b1 = Convert.ToInt32(Y1);
-            Console.WriteLine("Enter the Co-ordinates of second point as B(x2,y2) : ");
-            X2 = Console.ReadLine();
-            a2 = Convert.ToInt32(X2);
-            Y2 = Console.ReadLine();
-            b2 = Convert.ToInt32(Y2);
-
-            Console.WriteLine("The Distance between two points is : " + Math.Round(distance(a1, b1, a2, b2) * 100000.0) / 100000.0);
+            Console.WriteLine("Welcome to Line Comparision Computation Program");
+            LineCalculation ln = new LineCalculation(9.0, 1.0, 3.0, 5.0);
+            double lengthOne = ln.lengthCalculation();
+            Console.WriteLine("The Length of the First Line is {0}", lengthOne);
+            double lengthTwo = ln.lengthCalculation();
+            Console.WriteLine("The Length of Second Line is {0}", lengthTwo);
+            ln.Check(lengthOne, lengthTwo);
         }
-        static double distance(int x1, int y1, int x2, int y2)
-        {
-            
-            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) * 1.0);
-        }
-
     }
 }
